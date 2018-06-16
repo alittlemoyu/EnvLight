9e6713b2de15d76b4190578807899355922c85d6
Chery Sun <alittlemoyu@gmail.com>
2018-06-16 15:59:37 +0800

    Add files via upload

---
+++ TempSensor.ino
@@ -0,0 +1,12 @@
+float temp;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  temp = analogRead(A1)*5*100/1024.00;
+  Serial.println(temp);
+  delay(300);
+}
